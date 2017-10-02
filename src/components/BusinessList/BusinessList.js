a026e12df8778b0b4868440e9a25004e957bc156
Pete Akhurst <peteakhurst@gmail.com>
2017-10-03 09:27:25 +1100

    new commit

--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -7,7 +7,7 @@ import Business from '../Business/Business';
 class BusinessList extends React.Component{
   render() {
     return (
-    <div className={BusinessList}>
+    <div className="BusinessList">
     <Business />
     <Business />
     <Business />
